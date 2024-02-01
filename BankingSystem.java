35db57040db8ce038ee5b0b920fa6629bbe68983
Dell <abdullahanwar133@gmail.com>
2024-02-01 12:08:11 +0600

    update, withdraw done and the file is in-place updated

--- BankingSystem.java
+++ BankingSystem.java
@@ -1,14 +1,17 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.sql.Date;
 import java.util.Random;
 import java.util.Scanner;
 
 public class BankingSystem {
-  
+
     private static final String FILE_NAME = "accounts.txt";
     private static final String DELIMITER = ",";
     private static final String NEW_LINE_SEPARATOR = "\n";
@@ -120,7 +123,72 @@ public class BankingSystem {
     }
 
     public void updateAccount() {
-        
+        try{
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter account number:");
+            String accountNumber = scanner.nextLine();
+            System.out.println("Enter id:");
+            String id = scanner.nextLine();
+            String accountDetails[] = findAccount(accountNumber, id);
+            if (accountDetails == null) {
+                throw new Exception("Account not found");
+            }
+            // which field to update?
+            System.out.println("Which field do you want to update?");
+            System.out.println("1. Account holder name");
+            System.out.println("2. Account holder's id");
+            System.out.println("Enter your choice:");
+            String choice = scanner.nextLine();
+
+            String updatedLine = "";
+            switch (choice) {
+                case "1":
+                    System.out.println("Enter new account holder name:");
+                    String accountHolderName = scanner.nextLine();
+                    accountDetails[1] = accountHolderName;
+                    updatedLine = String.join(DELIMITER, accountDetails);
+                    break;
+                case "2":
+                    System.out.println("Enter new account holder's id:");
+                    String accountHolderId = scanner.nextLine();
+                    accountDetails[2] = accountHolderId;
+                    updatedLine = String.join(DELIMITER, accountDetails);
+                    break;
+                
+                default:
+                    throw new Exception("Error: Invalid choice");
+            }
+
+            updateFile(accountNumber, updatedLine);
+
+
+        }
+        catch(Exception e){
+            System.out.println("Error updating account: " + e.getMessage());
+        }
+    }
+
+    private void updateFile(String accountNumber, String updatedLine) throws Exception{
+        File file = new File(FILE_NAME);
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+
+        String line;
+        long currentPosition = 0;
+        while ((line = randomAccessFile.readLine()) != null) {
+            String accountDetails[] = line.split(DELIMITER);
+            String number = accountDetails[3];
+            if (number.equals(accountNumber)) {
+                randomAccessFile.seek(currentPosition);
+
+                randomAccessFile.writeBytes(updatedLine);
+                break;
+            }
+
+            currentPosition = randomAccessFile.getFilePointer();
+        }
+
+        randomAccessFile.close();
+
     }
 
     public void deleteAccount() {
@@ -173,7 +241,7 @@ public class BankingSystem {
 
             String line = "";
             String updatedFileContent = "";
-            boolean accountFound = false; 
+            boolean accountFound = false;
             Double newBalance = 0.0;
 
             while ((line = bufferedReader.readLine()) != null) {
@@ -187,10 +255,10 @@ public class BankingSystem {
                 String balance = accountDetails[5];
 
                 if (number.equals(accountNumber)) {
-                    accountFound = true; 
+                    accountFound = true;
                     System.out.println("Enter amount to deposit:");
                     Double amount = scanner.nextDouble();
-                     newBalance = Double.parseDouble(balance) + amount;
+                    newBalance = Double.parseDouble(balance) + amount;
 
                     updatedFileContent += type + DELIMITER + name + DELIMITER + id + DELIMITER + number + DELIMITER
                             + openingDate + DELIMITER + newBalance + NEW_LINE_SEPARATOR;
@@ -212,14 +280,82 @@ public class BankingSystem {
             bufferedWriter.close();
             fileWriter.close();
 
-            System.out.println("Amount deposited successfully! New balance is "+ newBalance + "\n"+ updatedFileContent);
+            System.out
+                    .println("Amount deposited successfully! New balance is " + newBalance + "\n" + updatedFileContent);
         } catch (Exception e) {
             System.out.println("Error depositing amount: " + e.getMessage());
         }
     }
 
     public void withdrawAmount() {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter account number:");
+            String accountNumber = scanner.nextLine();
+            System.out.println("Enter id:");
+            String id = scanner.nextLine();
+            String accountDetails[] = findAccount(accountNumber, id);
+            if (accountDetails == null) {
+                throw new Exception("Account not found");
+            }
 
+            System.out.println("Enter amount to withdraw:");
+            Double amount = scanner.nextDouble();
+            // check if after deducting amount, balance is less than minimum balance
+            Double balance = Double.parseDouble(accountDetails[5]);
+            if (balance - amount < MINIMUM_BALANCE) {
+                throw new Exception("Error: Minimum balance should be " + MINIMUM_BALANCE);
+            }
+
+            Double newBalance = balance - amount;
+
+            updateFile(accountNumber, newBalance);
+
+        } catch (Exception e) {
+            System.out.println("Error withdrawing amount: " + e.getMessage());
+        }
+
+    }
+
+    private void updateFile(String userAccountNumber, Double newBalance) throws Exception {
+        File file = new File(FILE_NAME);
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "rw");
+
+        String line;
+        long currentPosition = 0;
+        while ((line = randomAccessFile.readLine()) != null) {
+            String accountDetails[] = line.split(DELIMITER);
+            String number = accountDetails[3];
+            if (number.equals(userAccountNumber)) {
+                
+                accountDetails[5] = String.valueOf(newBalance);
+                
+
+                randomAccessFile.seek(currentPosition);
+                String updatedLine = String.join(DELIMITER, accountDetails);
+                randomAccessFile.writeBytes(updatedLine);
+                break;
+            }
+
+            currentPosition = randomAccessFile.getFilePointer();
+        }
+
+        randomAccessFile.close();
+    }
+
+    private String[] findAccount(String userAccountNumber, String userId) throws Exception {
+        FileReader fileReader = new FileReader(FILE_NAME);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] accountDetails = line.split(DELIMITER);
+            String number = accountDetails[3];
+            String id = accountDetails[2];
+            if (number.equals(userAccountNumber) && id.equals(userId)) {
+                return accountDetails;
+            }
+        }
+        return null;
     }
 
     public void searchAccount() {
@@ -227,42 +363,39 @@ public class BankingSystem {
             Scanner scanner = new Scanner(System.in);
             System.out.println("Enter account number:");
             String accountNumber = scanner.nextLine();
-
-            FileReader fileReader = new FileReader(FILE_NAME);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-            String line = "";
-            boolean accountFound = false; 
-
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] accountDetails = line.split(DELIMITER);
-
-                String type = accountDetails[0];
-                String name = accountDetails[1];
-                String id = accountDetails[2];
-                String number = accountDetails[3];
-                String openingDate = accountDetails[4];
-                String balance = accountDetails[5];
-
-                if (number.equals(accountNumber)) {
-                    accountFound = true; 
-                    System.out.printf("%-25s %-25s %-25s %-25s %-25s %-25s\n", "Type", "Name", "ID", "Number",
-                    "Account Opening Date", "Balance");
-                    System.out.println("-".repeat(150));
-                    System.out.printf("%-25s %-25s %-25s %-25s %-25s %-25s\n", type, name, id, number, openingDate,
-                            balance);
-                } 
+            String accountDetails[] = findAccount(accountNumber);
+            if (accountDetails == null) {
+                throw new Exception("Account not found");
             }
 
-            bufferedReader.close();
-            fileReader.close();
+            String type = accountDetails[0];
+            String name = accountDetails[1];
+            String id = accountDetails[2];
+            String number = accountDetails[3];
+            String openingDate = accountDetails[4];
+            String balance = accountDetails[5];
 
-            if (!accountFound) {
-                throw new Exception(" Account not found");
-            }
+            System.out.println("-".repeat(150));
+            System.out.printf("%-25s %-25s %-25s %-25s %-25s %-25s\n", type, name, id, number, openingDate,
+                    balance);
+            System.out.println("-".repeat(150));
 
         } catch (Exception e) {
             System.out.println("Error searching account: " + e.getMessage());
         }
+    }
+
+    private String[] findAccount(String accountNumber) throws Exception {
+        FileReader fileReader = new FileReader(FILE_NAME);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] accountDetails = line.split(DELIMITER);
+            String number = accountDetails[3];
+            if (number.equals(accountNumber)) {
+                return accountDetails;
+            }
+        }
+        return null;
     }
 }
